00dec27e1dbf5718b835ea44fedb652689e85384
Cathy Li <cathylilingwai@gmail.com>
2024-01-13 22:05:05 +0800

    update with SQLlTE

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
-from application import routes
+# Configuring the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expsenseDB.db'
+
+
+# Load the environment variables
+load_dotenv()
+
+# Access the SECRET_KEY using the environment variable
+secret_key = os.getenv("API_KEY")
+
+app.config['API_KEY'] = secret_key
+
+db = SQLAlchemy(app)
+
+#from application import routes
